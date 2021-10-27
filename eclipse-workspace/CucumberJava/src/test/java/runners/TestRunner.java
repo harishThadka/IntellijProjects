6673cdbc0609635fc66f0413412925210f12132c
harishThadka <thadkaharish@gmail.com>
2021-10-26 21:08:53 -0500

    removed unnecessary files

--- eclipse-workspace/CucumberJava/src/test/java/runners/TestRunner.java
+++ eclipse-workspace/CucumberJava/src/test/java/runners/TestRunner.java
@@ -1,5 +1,12 @@
 package runners;
 
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/Features",glue= {"src/test/java/StepDefinitions"})
 public class TestRunner {
 
 }
